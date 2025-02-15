8e2e97db902fe1bcf3516db90900bf810cefeab7
Anthony Ashcraft <anthonyashcraft91@gmail.com>
2025-02-14 22:11:40 -0500

    fixed video player

--- frontend/src/pages/Course.jsx
+++ frontend/src/pages/Course.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import { Flex, Box, Text, Container } from "@chakra-ui/react";
 import { EmbedPDF } from "@simplepdf/react-embed-pdf";
+import ReactPlayer from "react-player";
 
 export default function Course({ singleCourse, getCourse }) {
   const params = useParams();
@@ -12,19 +13,42 @@ export default function Course({ singleCourse, getCourse }) {
 
   return (
     <Container display="flex" justifyContent="center">
-      <div>
+      {singleCourse?.data?.contentType === "PDF" ? (
+        <iframe src={singleCourse?.data?.englishLink}></iframe>
+      ) : singleCourse?.data?.contentType === "Video" ? (
         <div
           style={{
-            paddingTop: "56.25%",
             position: "relative",
-            backgroundColor: "yellow",
             width: "100%",
-            height: "100%"
+            height: "0",
+            paddingTop: "56.2500%",
+            paddingBottom: "0",
+            boxShadow: "0 2px 8px 0 rgba(63,69,81,0.16)",
+            marginTop: "1.6em",
+            marginBottom: "0.9em",
+            overflow: "hidden",
+            borderRadius: "8px",
+            willChange: "transform"
           }}
         >
-          <iframe src={singleCourse?.data?.englishLink}></iframe>
+          <iframe
+            loading="lazy"
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              top: "0",
+              left: "0",
+              border: "none",
+              padding: "0",
+              margin: "0"
+            }}
+            src={`${singleCourse?.data?.englishLink}?embed`}
+          ></iframe>
         </div>
-      </div>
+      ) : (
+        ""
+      )}
     </Container>
   );
 }
